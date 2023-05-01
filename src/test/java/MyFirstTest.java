54cfa06985aa4479460a43ebf762ed7340685022
Lennox Ballard <noellemb@gmail.com>
2023-05-01 10:18:29 -0500

    tessts

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
@@ -30,4 +33,49 @@ public class MyFirstTest {
     public void testObjectSameness(){
         assertNotSame(bird2, bird3);
     }
+
+    //reate a test method which uses assertEquals or assertNotEquals to verify if the String Codeup is the same as CodeUp.
+    @Test
+    public void testString(){
+        String input = "Codeup";
+        String input2 = "CodeUp";
+
+        assertNotEquals(input, input2);
+    }
+
+    @Test
+    public void testArrayList(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+        assertNotSame(languages, moreLanguages);
+    }
+
+
+    @Test
+    public void testArray(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
+    @Test
+    public void testString2(){
+        String language = "PHP";
+        assertTrue(true);
+        assertFalse(false);
+    }
+
+    @Test
+    public void testString3(){
+        String language = null;
+        assertNull(language);
+
+        language = "Java";
+        assertNotNull(language);
+    }
+
 }
